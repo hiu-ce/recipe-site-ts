c4c0e0cfef57292f90ea9c8a68a07ef2dcf85b61
chocoboy08 <chocoboy0508@gmail.com>
2023-03-15 17:12:47 +0900

    search_3/15

--- src/App.tsx
+++ src/App.tsx
@@ -8,28 +8,46 @@ import { Box, ImageListItemBar } from '@mui/material';
 import Title from './components/Title';
 import SearchBar from './components/SearchBar';
 
+export type searchData = {
+  title:string | null,
+  img:string | null
+};
+
 function App() {
   const data = db.recipes.map((item) => ({
     img: item.src,
     title: item.name,
   }));
+  const [value, setValue] = React.useState<string|null>(null);
   return (
     <div>
       <Title>My Recipes</Title>
-      <SearchBar data={data} />
+      <SearchBar data={data} value={value} setValue={setValue}/>
       <Box sx={{ width: 500, height: 450, overflowY: 'scroll' }}>
-        <ImageList variant="masonry" cols={3} gap={8}>
-          {data.map((item) => (
-            <ImageListItem key={item.img}>
+        <ImageList  cols={3} gap={8}>
+          {
+            value === null ? data.map((item) => (
+              <ImageListItem key={item.img} >
               <img
                 src={`${item.img}?w=248&fit=crop&auto=format`}
                 srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
                 alt={item.title}
                 loading="lazy"
               />
-              <ImageListItemBar position="below" title={item.title} />
+              <ImageListItemBar position='below' title={item.title} />
             </ImageListItem>
-          ))}
+            )) : data.map((item) => (
+              value === item.title ? <ImageListItem key={item.img}>
+              <img
+                src={`${item.img}?w=248&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading="lazy"
+              />
+              <ImageListItemBar position='below' title={item.title} />
+            </ImageListItem> : null
+            ))
+          }
         </ImageList>
       </Box>
     </div>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,18 +1,33 @@
 import { Autocomplete, TextField } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
+import { server } from 'typescript';
+import { searchData } from '../App';
 import { RecipeImageWithTitle } from '../types/recipes';
 
 export interface SearchBarProps {
   data: RecipeImageWithTitle;
+  value: string|null;
+  setValue: React.Dispatch<React.SetStateAction<string|null>>;
 }
 
 function SearchBar(props: SearchBarProps) {
   const { data } = props;
+  const [inputValue, setInputValue] = useState<string | undefined>('');
+  const handleOnChange = (event : React.SyntheticEvent, value : string | null) => {
+    props.setValue(value);
+  }
+  const handleOnInputChange= (event : React.SyntheticEvent, value : string | undefined) =>{
+    setInputValue(value);
+  }
   return (
     <Autocomplete
       id="free-solo-demo"
       freeSolo
       options={data.map((option) => option.title)}
+      value={props.value}
+      inputValue={inputValue}
+      onInputChange={handleOnInputChange}
+      onChange={handleOnChange}
       renderInput={(params) => (
         <TextField {...params} label="검색어를 입력하세요" />
       )}
